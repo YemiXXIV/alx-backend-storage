771f0081cab240b326e315ef939b7b20d70c65eb
Mustapha <mustapha.yinusa@yahoo.com>
2024-08-30 01:14:06 +0100

    Redis tasks

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+To write to redis
+"""
+import redis
+from uuid import uuid4
+from typing import Union
+
+
+class Cache():
+    """
+    write to redis
+    """
+    def __init__(self) -> None:
+        """
+        initialize redis in class
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        get uuid key and save data
+        """
+        keystore = str(uuid4())
+        self._redis.set(keystore, data)
+        return keystore
